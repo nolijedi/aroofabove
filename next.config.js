a25b8f8240263bafcb82253aefcdd9127e0918c1
nolijedi <robbainstax@gmail.com>
2025-02-12 12:27:34 -0800

    Update Next.js config for custom domain

--- next.config.js
+++ next.config.js
@@ -2,7 +2,36 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-  output: 'standalone'
+  output: 'standalone',
+  async rewrites() {
+    return {
+      beforeFiles: [
+        {
+          source: '/:path*',
+          has: [
+            {
+              type: 'host',
+              value: 'site.aroofabove.co',
+            },
+          ],
+          destination: '/:path*',
+        },
+      ],
+    }
+  },
+  async headers() {
+    return [
+      {
+        source: '/:path*',
+        headers: [
+          {
+            key: 'X-DNS-Prefetch-Control',
+            value: 'on'
+          }
+        ],
+      },
+    ]
+  }
 }
 
 module.exports = nextConfig
